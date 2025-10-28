6d3e696cb25695303fa5d15303947e9bba1c0656
Beepered <bktrieu@gmail.com>
2025-10-28 09:41:17 -0700

    random small changes

--- src/prefabs/Player.js
+++ src/prefabs/Player.js
@@ -1,130 +1,135 @@
-class Player extends Phaser.Physics.Arcade.Sprite{
-    constructor(scene, x, y, texture, frame){
-        super(scene, x, y, texture, frame);
-        scene.add.existing(this)
-        scene.physics.add.existing(this);
-        this.body.setSize(20, 48)
-        this.setCollideWorldBounds(true)
-        
-        //stats
-        this.movement_speed = 160;
-        this.jump_height = -650
-        this.gravity = 18
-        this.isJumping = false; this.alive = true;
-        bullet = new Bullet(scene, -10, -10, "bullet")
-        this.canShoot = true; this.invincible = false
-        this.direction = 1 //direction player is facing so the bullet knows where to go
+class Player extends Phaser.Physics.Arcade.Sprite {
+  constructor(scene, x, y, texture, frame) {
+    super(scene, x, y, texture, frame);
+    scene.add.existing(this);
+    scene.physics.add.existing(this);
+    this.body.setSize(20, 48);
+    this.setCollideWorldBounds(true);
 
-        //adding sounds and animation
-        this.jumpSound = scene.sound.add("jump") 
-        this.shootSound = scene.sound.add("shoot")
-        this.hurtSound = scene.sound.add("groom_hurt")
-        this.anims.create({
-            key: "idle",
-            frames: this.anims.generateFrameNumbers("player", {
-                start: 2,
-                end: 2
-            })
-        })
-        this.anims.create({
-            key: "move",
-            frameRate: 4,
-            repeat: -1,
-            frames: this.anims.generateFrameNumbers("player", {
-                start: 0,
-                end: 1
-            })
-        })
-        this.anims.create({
-            key: "shoot",
-            frames: this.anims.generateFrameNumbers("player", {
-                start: 3,
-                end: 3
-            })
-        })
+    //stats
+    this.movement_speed = 160;
+    this.jump_height = -465;
+    this.gravity = 18;
+    this.isJumping = false;
+    this.alive = true;
+    bullet = new Bullet(scene, -10, -10, "bullet");
+    this.canShoot = true;
+    this.invincible = false;
+    this.direction = 1; //direction player is facing so the bullet knows where to go
+
+    //adding sounds and animation
+    this.jumpSound = scene.sound.add("jump");
+    this.shootSound = scene.sound.add("shoot");
+    this.hurtSound = scene.sound.add("groom_hurt");
+    this.anims.create({
+      key: "idle",
+      frames: this.anims.generateFrameNumbers("player", {
+        start: 2,
+        end: 2,
+      }),
+    });
+    this.anims.create({
+      key: "move",
+      frameRate: 4,
+      repeat: -1,
+      frames: this.anims.generateFrameNumbers("player", {
+        start: 0,
+        end: 1,
+      }),
+    });
+    this.anims.create({
+      key: "shoot",
+      frames: this.anims.generateFrameNumbers("player", {
+        start: 3,
+        end: 3,
+      }),
+    });
+  }
+  update(time, delta) {
+    if (this.alive) {
+      this.movement();
+      this.shoot();
     }
-    update(){
-        if(this.alive){
-            this.movement()
-            this.shoot()
-        }
-        this.body.velocity.y += this.gravity
-    }
+    this.body.velocity.y += this.gravity;
+  }
 
-    movement(){
-        if(this.body.touching.down){ //preventing "double" jumping
-            this.isJumping = false
-        }
-        else{
-            this.isJumping = true
-        }
-
-        //movement
-        if (keyLEFT.isDown) {
-            this.direction = -1
-            this.body.velocity.x = -this.movement_speed
-            this.flipX = true;
-            if (this.canShoot) { //play move animation only when not shooting
-                this.play("move", true)
-            }
-        }
-        else if (keyRIGHT.isDown) {
-            this.direction = 1
-            this.body.velocity.x = this.movement_speed
-            this.flipX = false;
-            if (this.canShoot) {
-                this.play("move", true)
-            }
-        }
-        else {
-            this.body.velocity.x = 0
-            if (this.canShoot) {
-                this.play("idle")
-            }
-        }
-
-        if(Phaser.Input.Keyboard.JustDown(keyUP) && !this.isJumping){ //jump
-            this.jumpSound.play()
-            this.body.velocity.y = this.jump_height
-        }
+  movement() {
+    if (this.body.touching.down) {
+      //preventing "double" jumping
+      this.isJumping = false;
+    } else {
+      this.isJumping = true;
     }
 
-    shoot(){
-        if(Phaser.Input.Keyboard.JustDown(SPACEBAR) && this.canShoot){ //shooting
-            this.play("shoot")
-            this.shootSound.play()
-            this.canShoot = false
-            bullet.shoot(this.x + (15 * this.direction), this.y - 10, this.direction);
-            this.scene.time.delayedCall(500, () => {
-                this.canShoot = true
-            })
-        }
+    //movement
+    if (keyLEFT.isDown) {
+      this.direction = -1;
+      this.body.velocity.x = -this.movement_speed;
+      this.flipX = true;
+      if (this.canShoot) {
+        //play move animation only when not shooting
+        this.play("move", true);
+      }
+    } else if (keyRIGHT.isDown) {
+      this.direction = 1;
+      this.body.velocity.x = this.movement_speed;
+      this.flipX = false;
+      if (this.canShoot) {
+        this.play("move", true);
+      }
+    } else {
+      this.body.velocity.x = 0;
+      if (this.canShoot) {
+        this.play("idle");
+      }
     }
 
-    killed(){
-        if(!this.invincible){
-            this.hurtSound.play()
-            lives--
-            this.alive = false
-            this.invincible = true
-            this.body.velocity.x = 0
-            this.body.velocity.y = -170 //jump up a little
-            this.anims.stop();
-            this.scene.time.delayedCall(1800, () => {
-                this.invincible = false
-            })
-            this.scene.time.delayedCall(1300, () => { //have some time before giving control back to player
-                if (lives == 0) { //no more lives
-                    if (score > parseInt(localStorage.getItem('highscore'))) { //save highscore to local storage
-                        localStorage.setItem('highscore', score)
-                    }
-                    this.scene.music.stop();
-                    this.scene.scene.start("menuScene");
-                    this.scene.scene.stop("UIScene")
-                }
-                this.alive = true
-            });
-        }
+    if (Phaser.Input.Keyboard.JustDown(keyUP) && !this.isJumping) {
+      //jump
+      this.jumpSound.play();
+      this.body.velocity.y = this.jump_height;
     }
-    
+  }
+
+  shoot() {
+    if (Phaser.Input.Keyboard.JustDown(SPACEBAR) && this.canShoot) {
+      //shooting
+      this.play("shoot");
+      this.shootSound.play();
+      this.canShoot = false;
+      bullet.shoot(this.x + 15 * this.direction, this.y - 10, this.direction);
+      this.scene.time.delayedCall(500, () => {
+        this.canShoot = true;
+      });
+    }
+  }
+
+  killed() {
+    if (!this.invincible) {
+      this.hurtSound.play();
+      lives--;
+      this.alive = false;
+      this.invincible = true;
+      this.body.velocity.x = 0;
+      this.body.velocity.y = -170; //jump up a little
+      this.anims.stop();
+      this.scene.time.delayedCall(2000, () => {
+        this.invincible = false;
+      });
+      this.scene.time.delayedCall(1250, () => {
+        //have some time before giving control back to player
+        if (lives == 0) {
+          //no more lives
+          if (score > parseInt(localStorage.getItem("highscore"))) {
+            //save highscore to local storage
+            localStorage.setItem("highscore", score);
+          }
+          this.scene.music.stop();
+          this.scene.scene.start("menuScene");
+          this.scene.scene.stop("UIScene");
+        }
+        this.alive = true;
+      });
+    }
+  }
 }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,160 +1,175 @@
-class Play extends Phaser.Scene{
-    constructor(){
-        super("playScene")
+class Play extends Phaser.Scene {
+  constructor() {
+    super("playScene");
+  }
+
+  preload() {
+    this.load.spritesheet("player", "assets/groom_spritesheet.png", {
+      frameWidth: 55,
+      frameHeight: 50,
+    });
+    this.load.image("bullet", "assets/bullet.png");
+
+    this.load.spritesheet("bride", "assets/bride_spritesheet.png", {
+      frameWidth: 55,
+      frameHeight: 50,
+    });
+    this.load.image("surprise", "assets/surprise.png");
+
+    this.load.image("ground", "assets/ground.png");
+    this.load.image("background", "assets/background.png");
+
+    this.load.spritesheet("police", "assets/police.png", {
+      frameWidth: 38,
+      frameHeight: 50,
+    });
+    this.load.spritesheet("heli-police", "assets/heli-police.png", {
+      frameWidth: 38,
+      frameHeight: 50,
+    });
+
+    this.load.spritesheet("person_woman", "assets/person_woman.png", {
+      frameWidth: 37,
+      frameHeight: 50,
+    });
+    this.load.spritesheet("person_man", "assets/person_man.png", {
+      frameWidth: 34,
+      frameHeight: 50,
+    });
+
+    this.load.audio("music", "assets/music.mp3");
+    this.load.audio("jump", "assets/jump.wav");
+    this.load.audio("shoot", "assets/shoot.wav");
+    this.load.audio("hit", "assets/hit.wav");
+    this.load.audio("groom_hurt", "assets/groom_hurt.mp3");
+    this.load.audio("bride_hurt", "assets/bride_hurt.mp3");
+  }
+
+  create() {
+    this.scene.launch("UIScene"); //display lives and points at top of screen
+    this.music = this.sound.add("music", {
+      volume: 0.1,
+      loop: true,
+    });
+    this.music.play();
+
+    this.background = this.add.image(0, 40, "background").setOrigin(0);
+    worldWidth = this.background.width;
+
+    //setting camera and collision based on size of background
+    this.cameras.main.setBounds(0, 0, worldWidth, 170);
+    this.physics.world.setBounds(0, 0, worldWidth, 170);
+
+    bride = new Bride(this, worldWidth / 2 - 50, 125, "bride", 3);
+
+    player = new Player(this, worldWidth / 2, 125, "player", 2);
+    this.cameras.main.startFollow(player, false).setZoom(3.5, 3.5); //let camera follow player
+
+    //  ground
+    let ground = this.physics.add.sprite(0, 150, "ground").setOrigin(0);
+    ground.setImmovable(true);
+    this.physics.add.collider(player, ground);
+
+    //increase difficulty every 15 seconds
+    this.difficultyTimer = this.time.addEvent({
+      delay: 15000,
+      callback: this.levelUp,
+      callbackScope: this,
+      repeat: 12,
+    });
+
+    //  COLLISION
+    this.physics.add.collider(bullet, bride, () => {
+      bullet.reset();
+      bride.killed();
+      player.killed();
+    });
+
+    //  enemy spawn and data
+    this.minimum_spawn_time = 1.4;
+    this.variation_spawn_time = 1.5;
+    this.spawn_time =
+      this.minimum_spawn_time + Math.random() * this.variation_spawn_time;
+    this.enemy_health = 0.1;
+    this.enemy_speed = 60;
+
+    this.EnemyGroup = this.add.group({
+      runChildUpdate: true,
+    });
+    this.physics.add.overlap(player, this.EnemyGroup, () => {
+      player.killed();
+    });
+
+    this.physics.add.overlap(bullet, this.EnemyGroup, (bullet, enemy) => {
+      bullet.reset();
+      enemy.damage(bullet);
+    });
+
+    //people spawn
+    this.minimum_person_spawn_time = 6;
+    this.variation_person_spawn_time = 3;
+    this.person_spawn_time =
+      this.minimum_person_spawn_time +
+      Math.random() * this.variation_person_spawn_time;
+
+    this.PersonGroup = this.add.group({
+      runChildUpdate: true,
+    });
+    this.physics.add.collider(bullet, this.PersonGroup, (bullet, person) => {
+      bullet.reset();
+      person.killed();
+    });
+
+    //controls
+    keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+    keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+    keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+    SPACEBAR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    this.keyESC = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.ESC
+    );
+  }
+
+  addEnemy() {
+    let enemy = new Enemy(this, this.enemy_health, this.enemy_speed);
+    this.EnemyGroup.add(enemy);
+    this.spawn_time =
+      this.minimum_spawn_time + Math.random() * this.variation_spawn_time;
+  }
+
+  addPerson() {
+    let person = new Person(this);
+    this.PersonGroup.add(person);
+    this.person_spawn_time =
+      this.minimum_person_spawn_time +
+      Math.random() * this.variation_person_spawn_time;
+  }
+
+  levelUp() {
+    this.minimum_spawn_time -= 0.08;
+    this.variation_spawn_time -= 0.09;
+    this.enemy_speed += 7;
+    this.enemy_health += 0.2;
+  }
+
+  update(time, delta) {
+    player.update();
+    bride.update();
+    bullet.update();
+
+    this.spawn_time -= delta * 0.001;
+    if (this.spawn_time <= 0) {
+      this.addEnemy();
+    }
+    this.person_spawn_time -= delta * 0.001;
+    if (this.person_spawn_time <= 0) {
+      this.addPerson();
     }
 
-    preload(){
-        this.load.spritesheet("player", "assets/groom_spritesheet.png", {
-            frameWidth: 55,
-            frameHeight: 50
-        })
-        this.load.image("bullet", "assets/bullet.png")
-
-        this.load.spritesheet("bride", "assets/bride_spritesheet.png", {
-            frameWidth: 55,
-            frameHeight: 50
-        })
-        this.load.image("surprise", "assets/surprise.png")
-
-        this.load.image("ground", "assets/ground.png")
-        this.load.image("background", "assets/background.png")
-
-        this.load.spritesheet("police", "assets/police.png", {
-            frameWidth: 38,
-            frameHeight: 50
-        })
-        this.load.spritesheet("heli-police", "assets/heli-police.png", {
-            frameWidth: 38,
-            frameHeight: 50
-        })
-
-        this.load.spritesheet("person_woman", "assets/person_woman.png", {
-            frameWidth: 37,
-            frameHeight: 50
-        })
-        this.load.spritesheet("person_man", "assets/person_man.png", {
-            frameWidth: 34,
-            frameHeight: 50
-        })
-
-        this.load.audio("music", "assets/music.mp3")
-        this.load.audio("jump", "assets/jump.wav")
-        this.load.audio("shoot", "assets/shoot.wav")
-        this.load.audio("hit", "assets/hit.wav")
-        this.load.audio("groom_hurt", "assets/groom_hurt.mp3")
-        this.load.audio("bride_hurt", "assets/bride_hurt.mp3")
+    if (this.keyESC.isDown) {
+      this.music.stop();
+      this.scene.start("menuScene");
+      this.scene.stop("UIScene");
     }
-
-    create(){
-        console.log(Phaser.VERSION)
-        this.scene.launch("UIScene") //display lives and points at top of screen
-        this.music = this.sound.add("music", {
-            volume: 0.1,
-            loop: true
-        });
-        this.music.play();
-
-        this.background = this.add.image(0, 40, "background").setOrigin(0)
-        worldWidth = this.background.width
-        
-        //setting camera and collision based on size of background
-        this.cameras.main.setBounds(0, 0, worldWidth, 170)
-        this.physics.world.setBounds(0, 0, worldWidth, 170)
-        
-        bride = new Bride(this, worldWidth / 2 - 50, 125, "bride", 3)
-        
-        player = new Player(this, worldWidth / 2, 125, "player", 2)
-        this.cameras.main.startFollow(player, false).setZoom(3.5, 3.5) //let camera follow player
-        
-        //  ground
-        let ground = this.physics.add.sprite(0, 150, "ground").setOrigin(0)
-        ground.setImmovable(true)
-        this.physics.add.collider(player, ground)
-
-        //increase difficulty every 15 seconds
-        this.difficultyTimer = this.time.addEvent({
-            delay: 15000,
-            callback: this.levelUp,
-            callbackScope: this,
-            repeat: 12
-        });
-
-        //  COLLISION
-        this.physics.add.collider(bullet, bride, ()=>{
-            bullet.reset()
-            bride.killed()
-            player.killed()
-        });
-
-        //  enemy spawn and data
-        this.minimum_spawn_time = 1.4
-        this.variation_spawn_time = 1.5
-        this.spawn_time = this.minimum_spawn_time + (Math.random() * this.variation_spawn_time)
-        this.enemy_health = 0.1; this.enemy_speed = 60
-
-        this.EnemyGroup = this.add.group({
-            runChildUpdate: true
-        })
-        this.physics.add.overlap(player, this.EnemyGroup, ()=>{
-            player.killed()
-        })
-
-        this.physics.add.overlap(bullet, this.EnemyGroup, (bullet, enemy) => {
-            bullet.reset()
-            enemy.damage(bullet)
-        })
-
-        //people spawn
-        this.minimum_person_spawn_time = 6
-        this.variation_person_spawn_time = 3
-        this.person_spawn_time = this.minimum_person_spawn_time + (Math.random() * this.variation_person_spawn_time)
-
-        this.PersonGroup = this.add.group({
-            runChildUpdate: true
-        })
-        this.physics.add.collider(bullet, this.PersonGroup, (bullet, person)=>{
-            bullet.reset()
-            person.killed()
-        });
-
-        //controls
-        keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
-        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
-        SPACEBAR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-    }
-
-    addEnemy(){
-        let enemy = new Enemy(this, this.enemy_health, this.enemy_speed)
-        this.EnemyGroup.add(enemy)
-        this.spawn_time = this.minimum_spawn_time + (Math.random() * this.variation_spawn_time)
-    }
-
-    addPerson(){
-        let person = new Person(this)
-        this.PersonGroup.add(person)
-        this.person_spawn_time = this.minimum_person_spawn_time + (Math.random() * this.variation_person_spawn_time)
-    }
-
-    levelUp(){
-        this.minimum_spawn_time -= 0.08;
-        this.variation_spawn_time -= 0.09;
-        this.enemy_speed += 7
-        this.enemy_health += 0.2
-    }
-
-    update(time, delta){
-        player.update();
-        bride.update();
-        bullet.update();
-        
-        this.spawn_time -= delta * 0.001
-        if(this.spawn_time <= 0){
-            this.addEnemy()
-        }
-        this.person_spawn_time -= delta * 0.001
-        if(this.person_spawn_time <= 0){
-            this.addPerson()
-        }
-    }
+  }
 }
